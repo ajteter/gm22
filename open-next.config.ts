79530bd835a44bdad0109809fade8aaa9132dc51
Steammmmm <ajteter@hotmail.com>
2025-09-11 15:14:47 +0800

    fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,6 @@
-const { defineCloudflareConfig } = require("@opennextjs/cloudflare");
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-module.exports = defineCloudflareConfig({
+export default defineCloudflareConfig({
   // Uncomment to enable R2 cache,
   // It should be imported as:
   // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
