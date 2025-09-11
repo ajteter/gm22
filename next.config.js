fa7c4e2cfa102d1394950c26ce31da18125aecbc
Steammmmm <ajteter@hotmail.com>
2025-09-11 14:35:19 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
 	},
 };
 
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+const { initOpenNextCloudflareForDev } = require("@opennextjs/cloudflare");
+initOpenNextCloudflareForDev();
+
 module.exports = nextConfig;
 
 
